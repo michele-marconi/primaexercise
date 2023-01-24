cd75ecc03701a6a80d0be869b82a76afa1e5a09c
Michele Marconi <michele.marconi@zehus.it>
2023-01-24 16:34:45 +0100

    Add @AndroidEntryPoint to main activity

--- app/src/main/java/com/example/prima_exercise/MainActivity.kt
+++ app/src/main/java/com/example/prima_exercise/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prima_exercise.ui.theme.PrimaexerciseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
